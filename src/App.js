64fc1ad50ca5964de8babc363c89f14991640629
Ali Chaghou <email@ali.supercode.gmail.com>
2023-01-08 19:17:31 +0100

    Import and Return Footer Component in App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 //Routing
 import { BrowserRouter} from "react-router-dom";
 
+//Components
+import Footer from './Components/Footer';
+
 function App() {
   return (
     <BrowserRouter>
         <div className="App">
 
+          <Footer/>
         </div>
     </BrowserRouter>
   );
