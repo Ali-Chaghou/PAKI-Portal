8ab4cac7b9882920fdbdc7c26bb9df5b7639a377
Ali Chaghou <email@ali.supercode.gmail.com>
2023-01-08 19:15:39 +0100

    Footer CSS Components Responsive styling

---
+++ src/Components/Footer.js
@@ -0,0 +1,17 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        © Copyright <span>Faber Group</span> 2023
+      </p>
+      <div>
+        <a href="#">Impressum</a>
+        <a href="#">Datenschutz- und Cookie-Erklärung</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
